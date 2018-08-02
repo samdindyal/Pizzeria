301eb211e2f352fccef92f413102e469f0a57d04
Sam Dindyal <sam.dindyal@me.com>
2018-08-02 19:11:03 -0400

    Dependency inject Pizzeria.

--- Pizzeria/AppDelegate.swift
+++ Pizzeria/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let navController = window?.rootViewController as! UINavigationController
+        let pizzeriaViewController = navController.topViewController as! PizzeriaViewController
+        pizzeriaViewController.pizzeria = Pizzeria()
         return true
     }
 
